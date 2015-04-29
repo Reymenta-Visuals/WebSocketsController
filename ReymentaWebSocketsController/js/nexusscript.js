0ce17c3e98012bf69fcd243d53938088a3132b5e
brucelane <bruce@batchass.fr>
2015-04-29 16:13:03 +0200

    nexus ws ok

--- ReymentaWebSocketsController/js/nexusscript.js
+++ ReymentaWebSocketsController/js/nexusscript.js
@@ -1,30 +1,13 @@
 ﻿nx.onload = function () {
 
 	nx.colorize("#0ac")
-	ghost1.watch()
-
-	tabs1.options = ["linear", "bounce", "random", "wander", "pattern"]
-	tabs1.init();
-
-	tabs1.on('*', function (data) {
-		ghost1.mode = data.text;
+	button1.on('press', function (data) {
+		console.log(data);
+		ws.send("button1");
 	})
-
-	dial7.on('*', function (data) {
-		ghost1.rate = data.value;
-	})
-
-	dial8.on('*', function (data) {
-		ghost1.noise = data.value;
-	})
-
-	range2.on('*', function (data) {
-		ghost1.start = data.start;
-		ghost1.end = data.stop;
-	})
-
 	position1.on('*', function (data) {
-		//	console.log(data);
+		console.log(data);
+		// data will be a float equal to the x coordinate of the 2D Position widget.
 	})
 
 }
